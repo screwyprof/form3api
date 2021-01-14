6a624cd764d626b9405607c81614471c260beb1d
Max O'Donnell <screwyprof@gmail.com>
2021-01-14 18:28:48 +0000

    Refactor tests

--- tests/e2e/account/accounts_test.go
+++ tests/e2e/account/accounts_test.go
@@ -3,8 +3,11 @@
 package account_test
 
 import (
+	"context"
 	"os"
 	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
 
 	"github.com/screwyprof/form3api"
 )
@@ -48,4 +51,32 @@ func assertIBAN(tb testing.TB, IBAN string) {
 func assertCustomer(tb testing.TB, customer string) {
 	tb.Helper()
 	form3api.True(tb, customer != "")
+}
+
+func createTestAccount(r form3api.CreateAccount) (*form3api.Account, error) {
+	return client.CreateAccount(context.Background(), r)
+}
+
+func generateCreateAccountRequest() form3api.CreateAccount {
+	r := form3api.CreateAccount{
+		AccountData: form3api.AccountData{
+			ID: gofakeit.UUID(),
+			OrganisationID: gofakeit.UUID(),
+			Type: "accounts",
+			Attributes: &form3api.AccountAttributes{
+				AccountNumber: gofakeit.Numerify("#########"),
+				BankID: gofakeit.Numerify("######"),
+				BankIDCode: "GBDSC",
+				Country: "GB",
+				Currency: "GBP",
+				CustomerID: "234",
+				IBAN: "GB28NWBK40030212764204",
+				BIC: "NWBKGB42",
+				ConfirmationOfPayee: &form3api.ConfirmationOfPayee{
+					AccountClassification: "Personal",
+				},
+			},
+		},
+	}
+	return r
 }
--- tests/e2e/account/fetch_account_test.go
+++ tests/e2e/account/fetch_account_test.go
@@ -6,8 +6,6 @@ import (
 	"context"
 	"testing"
 
-	"github.com/brianvoe/gofakeit/v6"
-
 	"github.com/screwyprof/form3api"
 )
 
@@ -27,33 +25,3 @@ func TestFetchAccount(t *testing.T) {
 	form3api.Ok(t, err)
 	form3api.Equals(t, want, acc)
 }
-
-func createTestAccount(r form3api.CreateAccount) (*form3api.Account, error) {
-	return client.CreateAccount(context.Background(), r)
-}
-
-func generateCreateAccountRequest() form3api.CreateAccount {
-	r := form3api.CreateAccount{
-		AccountData: form3api.AccountData{
-			ID: gofakeit.UUID(),
-			OrganisationID: gofakeit.UUID(),
-			Type: "accounts",
-			Attributes: &form3api.AccountAttributes{
-				AccountNumber: gofakeit.Numerify("#########"),
-				BankID: gofakeit.Numerify("######"),
-				BankIDCode: "GBDSC",
-				Country: "GB",
-				Currency: "GBP",
-				CustomerID: "234",
-				IBAN: "GB28NWBK40030212764204",
-				BIC: "NWBKGB42",
-				ConfirmationOfPayee: &form3api.ConfirmationOfPayee{
-					AccountClassification: "Personal",
-				},
-			},
-		},
-	}
-	return r
-}
-
-
